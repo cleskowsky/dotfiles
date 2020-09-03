768050364026c54f961c12e71b038797ed0c7783
Christian Leskowsky <christian@leskowsky.net>
2020-09-03 10:01:43 -0400

    Added brew scripts

---
+++ brew.sh
@@ -0,0 +1,12 @@
+brew tap mongodb/brew
+brew tap pivotal/tap
+
+brew install wget \
+  pstree \
+  the_silver_searcher \
+  ansible \
+  mongodb-community-shell \
+  springboot \
+  packer
+
+brew cask install iterm2 vagrant
---
+++ brew_once.sh
@@ -0,0 +1,3 @@
+# install homebrew
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
