bfd42c4e861f6dc0c6b27797502e3f409f7c2063
Christian Leskowsky <christian@leskowsky.net>
2022-01-13 23:15:39 -0500

    Removed packages that probably shouldn't be in my baseline setup script

--- bin/brew.sh
+++ bin/brew.sh
@@ -1,12 +1,6 @@
-brew tap mongodb/brew
-brew tap pivotal/tap
-
 brew install wget \
   pstree \
   the_silver_searcher \
-  ansible \
-  mongodb-community-shell \
-  springboot \
-  packer
+  ansible
 
-brew cask install iterm2 vagrant
+brew cask install iterm2
